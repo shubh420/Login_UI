12d78c7bf57c735d1c3df1def8928a0b7a038068
TarunPal3551 <tarunplay3551@gmail.com>
2019-02-04 21:35:59 +0530

    Initial commit

---
+++ app/src/main/java/io/shubh/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package io.shubh.myapplication;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+/*
+
+        getSupportActionBar().hide();
+
+//        getWindow().getDecorView().setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+     getWindow().setStatusBarColor(Color.TRANSPARENT);
+*/
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+
+        getSupportActionBar().hide();
+    }
+}
